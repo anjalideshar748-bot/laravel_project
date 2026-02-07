dcddbf52d963424aeb6e3174db33495ccab24247
Anjali <anjalideshar748@gmail.com>
2026-02-07 19:52:32 +0545

    .

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
             <thead>
                 <tr>
                     <th class="border-2">Name</th>
-                     <th class="border-2"> Phone</th>
+                     <th class="border-2">Phone</th>
                       <th class="border-2"> Email</th>
                        <th class="border-2"> Address</th>
                         <th class="border-2"> Logo</th>
